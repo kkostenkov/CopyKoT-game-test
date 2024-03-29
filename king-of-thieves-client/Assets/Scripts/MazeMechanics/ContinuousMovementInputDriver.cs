c4edf9655be29c93e65105949c7b2d4c893edd76
kkostenkov <kkostenkov@gmail.com>
2024-03-29 17:06:05 +0100

    Move Thief always

---
+++ king-of-thieves-client/Assets/Scripts/MazeMechanics/ContinuousMovementInputDriver.cs
@@ -0,0 +1,58 @@
+﻿using DYP;
+using UnityEngine;
+
+public class ContinuousMovementInputDriver : BaseInputDriver
+{
+    [SerializeField]
+    private BasicMovementController2D movementController;
+
+    private float horizontalMovement = 1;
+    private bool isWallSliding;
+
+    private void Awake()
+    {
+        movementController.OnWallSliding += OnWallSlide;
+        movementController.OnWallSlidingEnd += OnWallSlideEnd;
+    }
+
+    private void Update()
+    {
+        UpdateInput(Time.deltaTime);
+    }
+
+    private void OnDestroy()
+    {
+        movementController.OnWallSliding -= OnWallSlide;
+        movementController.OnWallSlidingEnd -= OnWallSlideEnd;
+    }
+
+    private void OnWallSlideEnd()
+    {
+        this.isWallSliding = false;
+    }
+
+    private void OnWallSlide(int obj)
+    {
+        this.isWallSliding = true;
+    }
+
+    public override void UpdateInput(float timeStep)
+    {
+        var isJumpPressed = Input.GetButtonDown("Jump");
+        Jump = isJumpPressed;
+        
+        if (isJumpPressed && this.isWallSliding) {
+            InvertHorizontalMovementDirection();
+        }
+
+        Horizontal = this.horizontalMovement;
+
+        HoldingJump = Input.GetButton("Jump");
+        ReleaseJump = Input.GetButtonUp("Jump");
+    }
+
+    private void InvertHorizontalMovementDirection()
+    {
+        this.horizontalMovement *= -1;
+    }
+}
