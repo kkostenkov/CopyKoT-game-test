6bcb8b3e884a54320c040f9977d2d594975994be
kkostenkov <kkostenkov@gmail.com>
2024-03-29 14:31:57 +0100

    Update build settings

--- king-of-thieves-client/Assets/Scripts/MazeMechanics/BaseCollectableController.cs
+++ king-of-thieves-client/Assets/Scripts/MazeMechanics/BaseCollectableController.cs
@@ -1,13 +1,10 @@
 using UnityEngine;
 
-namespace MazeMechanics
+public class BaseCollectableController : MonoBehaviour
 {
-    public class BaseCollectableController : MonoBehaviour
+    public void OnTriggerStay2D(Collider2D col)
     {
-        public void OnTriggerStay2D(Collider2D col)
-        {
-            Debug.Log("InCollectable trigger");
-            this.gameObject.SetActive(false);
-        }
+        Debug.Log("InCollectable trigger");
+        gameObject.SetActive(false);
     }
 }
