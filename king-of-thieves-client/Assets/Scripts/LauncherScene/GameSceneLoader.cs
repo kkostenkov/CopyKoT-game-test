925ad32fc5c3f3ff9d36b17d6172c9c329149287
kkostenkov <kkostenkov@gmail.com>
2024-03-31 22:23:53 +0200

    Set FPS to 60

--- king-of-thieves-client/Assets/Scripts/LauncherScene/GameSceneLoader.cs
+++ king-of-thieves-client/Assets/Scripts/LauncherScene/GameSceneLoader.cs
@@ -7,6 +7,9 @@ public class GameSceneLoader : MonoBehaviour
 {
     private async void Awake()
     {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
+        
         var delay = Application.isEditor ? 0 : 0.5f;
         await Task.Delay(TimeSpan.FromSeconds(delay));
 
