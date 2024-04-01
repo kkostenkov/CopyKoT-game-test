ee9155c9eaec20bea64ac4b5d9bd2f3b312b009f
kkostenkov <kkostenkov@gmail.com>
2024-04-01 22:42:50 +0200

    Create Settings scriptable object

---
+++ king-of-thieves-client/Assets/Scripts/LevelMechanics/GameSettingsScriptable.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenuAttribute(fileName = "GameSettings", menuName = "GameData/GameSettings")]
+public class GameSettingsScriptable : ScriptableObject
+{
+    public int ChestChancePercent = 10;
+    public int MinSpawnDelaySeconds = 3;
+    public int MaxSpawnDelaySeconds = 10;
+}
